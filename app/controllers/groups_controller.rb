d88d4f2dcc2b849ca459b39939cb6736cc8039e7
tatsuma0928 <tatsuma.sano@icloud.com>
2020-07-10 18:54:39 +0900

    suver-side-message No.2

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,7 +24,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
@@ -36,17 +36,3 @@ class GroupsController < ApplicationController
   end
 
 end
-
-  def update
-    @group = Group.find(params[:id])
-    if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
-    else
-      render :edit
-    end
-  end
-
-  private
-  def group_params
-    params.require(:group).permit(:name, user_ids: [] )
-  end
